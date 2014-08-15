e8b448569a7039dc72b496f599b7ef2bdf4fac81
Wouter Bolsterlee <uws@xs4all.nl>
2014-08-16 01:06:34 +0200

    Initial setup script

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='exceptional',
+    description="exception handling utilities",
+    version='0.1',
+    author="Wouter Bolsterlee",
+    author_email="uws@xs4all.nl",
+    url='https://github.com/wbolster/exceptional',
+    license="BSD",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+)
